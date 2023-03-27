2d5ff8d5fb90846dab416d5c285e8db5c4d67efb
Thibaut Goelff <thibaut.goelff@infrabel.be>
2023-03-27 18:55:44 +0200

    Test duckdb

--- utils.py
+++ utils.py
@@ -2,7 +2,8 @@ from pyproj import Transformer
 import pandas as pd
 
 def get_ptcars_l72():
-    df = pd.read_json('https://opendata.infrabel.be/api/explore/v2.1/catalog/datasets/operationele-punten-van-het-newterk/exports/json?lang=fr&timezone=Europe%2FBerlin')
+    # df = pd.read_json('https://opendata.infrabel.be/api/explore/v2.1/catalog/datasets/operationele-punten-van-het-newterk/exports/json?lang=fr&timezone=Europe%2FBerlin')
+    df = pd.read_json('operationele-punten-van-het-newterk.json', encoding='utf8')
     transformer = Transformer.from_crs("EPSG:4326", "EPSG:31370", always_xy=True)
     
     df['lat'] = df['geo_shape'].apply(lambda x: x['geometry']['coordinates'][1])
